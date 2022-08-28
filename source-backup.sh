17d5a91c87a95d0e1fd6b59f697b0c18a6df53cc
Gadgetmies <71213783+gadgetmies@users.noreply.github.com>
2022-08-28 22:11:51 +0300

    Update source-backup.sh
    * Direct all output to log file
    * Indent with tabs
    * Add hash bang

--- source-backup.sh
+++ source-backup.sh
@@ -1,8 +1,11 @@
-LOG_FILE=${:-"/var/log/tiny-backup-box.log"}
+#!/bin/sh
 
-# Back up previous log file
+LOG_FILE=${LOG_FILE:-"/var/log/tiny-backup-box.log"}
+
+# Backup previous log file
 mv $LOG_FILE $LOG_FILE.old || true
 
+{
 echo "### Starting SD card backup at $(date)"
 
 cat << EOF
@@ -30,7 +33,7 @@ echo "default-on" > /sys/class/leds/$STATUS_LED/trigger
 
 MOUNTED_STORAGE=`findmnt -rno SOURCE $STORAGE_MOUNT_POINT`
 if [ -z "$MOUNTED_STORAGE" ]; then
-  echo "Storage device not mounted, start polling..."
+	echo "Storage device not mounted, start polling..."
 
 	SLEEP_COUNT=0
 	until [[ -e "$STORAGE_DEV" ]]; do
@@ -41,11 +44,11 @@ if [ -z "$MOUNTED_STORAGE" ]; then
 		fi
 	done
 
-  echo "Found storage device in $STORAGE_DEV. Mounting to $STORAGE_MOUNT_POINT"
+	echo "Found storage device in $STORAGE_DEV. Mounting to $STORAGE_MOUNT_POINT"
 	mount "$STORAGE_DEV" "$STORAGE_MOUNT_POINT"
 else
-  echo "Storage device already mounted, continuing"
-  STORAGE_DEV="$MOUNTED_STORAGE"
+	echo "Storage device already mounted, continuing"
+	STORAGE_DEV="$MOUNTED_STORAGE"
 fi
 
 MOUNTED_SOURCE=`findmnt -rno SOURCE $SOURCE_MOUNT_POINT`
@@ -60,29 +63,28 @@ if [ -z "$MOUNTED_SOURCE" ]; then
 			exit 1
 		fi
 	fi
-	
 	echo "SD card not mounted, start polling..."
 
 	SLEEP_COUNT=0
 	until [[ -e "$SOURCE_DEV" ]]; do
-		sleep 1
 		let SLEEP_COUNT++
 		if [ $(expr $SLEEP_COUNT % 60) -eq 0 ]; then 
 			echo "Still polling..." 
 		fi
+		sleep 1
 	done
 
 	echo "Found SD card device in $SOURCE_DEV. Mounting to $SOURCE_MOUNT_POINT"
 	mount "$SOURCE_DEV" "$SOURCE_MOUNT_POINT"
 else
 	echo "SD card already mounted, continuing"
-  SOURCE_DEV="$MOUNTED_SOURCE"
+	SOURCE_DEV="$MOUNTED_SOURCE"
 fi
 
 cd $SOURCE_MOUNT_POINT
 if [ ! -f *.id ]; then
-    random=$(</dev/urandom tr -dc A-Za-z0-9-_ | head -c 22)
-    touch $(date +"%Y%m%d%H%M")-$random.id
+	random=$(</dev/urandom tr -dc A-Za-z0-9-_ | head -c 22)
+	touch $(date +"%Y%m%d%H%M")-$random.id
 fi
 ID_FILE=$(ls -t *.id | head -n1)
 ID="${ID_FILE%.*}"
@@ -92,9 +94,9 @@ cd
 BACKUP_PATH="$STORAGE_MOUNT_POINT"/"$ID"
 
 if [ $STOP_LUCI_FOR_BACKUP = true ]; then
-    echo "Stopping LUCI to free resources"
-    /etc/init.d/uhttpd stop
-    echo "LUCI stopped"
+	echo "Stopping LUCI to free resources"
+	/etc/init.d/uhttpd stop
+	echo "LUCI stopped"
 fi
 
 echo "Starting backup at $(date)"
@@ -105,27 +107,27 @@ echo "heartbeat" > /sys/class/leds/$STATUS_LED/trigger
 EXIT_CODE=1
 while [ ! $EXIT_CODE = 0 ]
 do
-    RSYNC_OUTPUT=$(rsync -avhW --stats --exclude "*.id" --log-file="$LOG_FILE" "$SOURCE_MOUNT_POINT"/ "$BACKUP_PATH")
-    EXIT_CODE=$?
-    if [ ! $EXIT_CODE = 0 ]; then
-      echo "Backup failed, restarting backup"
-    fi
-    sleep 1
+	RSYNC_OUTPUT=$(rsync -avhW --stats --exclude "*.id" --log-file="$LOG_FILE" "$SOURCE_MOUNT_POINT"/ "$BACKUP_PATH")
+	EXIT_CODE=$?
+	if [ ! $EXIT_CODE = 0 ]; then
+		echo "Backup failed, restarting backup"
+	fi
+	sleep 1
 done
 
 echo "Backup done at $(date)"
 
 if [ $STOP_LUCI_FOR_BACKUP = true ]; then
-    echo "Starting LUCI"
-    /etc/init.d/uhttpd start
-    echo "LUCI started"
+	echo "Starting LUCI"
+	/etc/init.d/uhttpd start
+	echo "LUCI started"
 fi
 
 echo "Unmounting SD card"
 umount "$SOURCE_MOUNT_POINT"
 
 if [ $UNMOUNT_STORAGE = true ]; then
-  umount "$STORAGE_MOUNT_POINT"
+	umount "$STORAGE_MOUNT_POINT"
 fi
 
 echo "none" > /sys/class/leds/$STATUS_LED/trigger
@@ -135,11 +137,12 @@ if [ $POWER_OFF = true ]; then
 	echo "Shutting down"
 	poweroff
 else
-  echo "default-on" > /sys/class/leds/$STATUS_LED/trigger
-  until [[ ! -e "$SOURCE_DEV" ]]; do
-    echo "Waiting for SD card removal..."
-    sleep 30
-  done
+	echo "default-on" > /sys/class/leds/$STATUS_LED/trigger
+	until [[ ! -e "$SOURCE_DEV" ]]; do
+		echo "Waiting for SD card removal..."
+		sleep 30
+	done
 
-  echo "SD card removed, restarting backup script"
+	echo "SD card removed, restarting backup script"
 fi
+} | tee -a $LOG_FILE
