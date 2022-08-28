b06949ecf9ff0f9a68260c02e0d8d355449712c1
Gadgetmies <71213783+gadgetmies@users.noreply.github.com>
2022-08-28 16:26:09 +0300

    Remove the tee commands
    Redirect the messages from the script to the log file already from the init script

--- source-backup.sh
+++ source-backup.sh
@@ -7,31 +7,31 @@ LOG_FILE=${LOG_FILE:-"/var/log/tiny-backup-box.log"}
 
 MOUNTED_STORAGE=`findmnt -rno SOURCE $STORAGE_MOUNT_POINT`
 if [ -z "$MOUNTED_STORAGE" ]; then
-  echo "Storage device not mounted, start polling..." | tee -a $LOG_FILE
+  echo "Storage device not mounted, start polling..."
 
 	until [[ -e "$STORAGE_DEV" ]]; do
 		sleep 1
 	done
 
-  echo "Found storage device in $STORAGE_DEV. Mounting to $STORAGE_MOUNT_POINT" | tee -a $LOG_FILE
+  echo "Found storage device in $STORAGE_DEV. Mounting to $STORAGE_MOUNT_POINT"
 	mount "$STORAGE_DEV" "$STORAGE_MOUNT_POINT"
 else
-  echo "Storage device already mounted, continuing" | tee -a $LOG_FILE
+  echo "Storage device already mounted, continuing"
   STORAGE_DEV="$MOUNTED_STORAGE"
 fi
 
 MOUNTED_SOURCE=`findmnt -rno SOURCE $SOURCE_MOUNT_POINT`
 if [ -z "$MOUNTED_SOURCE" ]; then
-	echo "SD card not mounted, start polling..." | tee -a $LOG_FILE
+	echo "SD card not mounted, start polling..."
 
 	until [[ -e "$SOURCE_DEV" ]]; do
 		sleep 1
 	done
 
-	echo "Found SD card device in $SOURCE_DEV. Mounting to $SOURCE_MOUNT_POINT" | tee -a $LOG_FILE
+	echo "Found SD card device in $SOURCE_DEV. Mounting to $SOURCE_MOUNT_POINT"
 	mount "$SOURCE_DEV" "$SOURCE_MOUNT_POINT"
 else
-	echo "SD card already mounted, continuing" | tee -a $LOG_FILE
+	echo "SD card already mounted, continuing"
   SOURCE_DEV="$MOUNTED_SOURCE"
 fi
 
@@ -50,12 +50,12 @@ BACKUP_PATH="$STORAGE_MOUNT_POINT"/"$ID"
 rm -f $LOG_FILE
 
 if [ $STOP_LUCI_FOR_BACKUP = true ]; then
-    echo "Stopping LUCI to free resources" | tee -a $LOG_FILE
+    echo "Stopping LUCI to free resources"
     /etc/init.d/uhttpd stop
-    echo "LUCI stopped" | tee -a $LOG_FILE
+    echo "LUCI stopped"
 fi
 
-echo "Starting backup at $(date)" | tee -a $LOG_FILE
+echo "Starting backup at $(date)"
 
 # The RP-WD03 can easily run out of memory crashing the backup. Thus loop until the backup is successful.
 EXIT_CODE=1
@@ -64,20 +64,20 @@ do
     RSYNC_OUTPUT=$(rsync -avh --stats --exclude "*.id" --log-file="$LOG_FILE" "$SOURCE_MOUNT_POINT"/ "$BACKUP_PATH")
     EXIT_CODE=$?
     if [ ! $EXIT_CODE = 0 ]; then
-      echo "Backup failed, restarting backup" | tee -a $LOG_FILE
+      echo "Backup failed, restarting backup"
     fi
     sleep 1
 done
 
-echo "Backup done at $(date)" | tee -a $LOG_FILE
+echo "Backup done at $(date)"
 
 if [ $STOP_LUCI_FOR_BACKUP = true ]; then
-    echo "Starting LUCI" | tee -a $LOG_FILE
+    echo "Starting LUCI"
     /etc/init.d/uhttpd start
-    echo "LUCI started" | tee -a $LOG_FILE
+    echo "LUCI started"
 fi
 
-echo "Unmounting SD card" | tee -a $LOG_FILE
+echo "Unmounting SD card"
 umount "$SOURCE_MOUNT_POINT"
 
 if [ $UNMOUNT_STORAGE = true ]; then
@@ -86,13 +86,13 @@ fi
 
 # Power off
 if [ $POWER_OFF = true ]; then
-	echo "Shutting down" | tee -a $LOG_FILE
+	echo "Shutting down"
 	poweroff
 else
   until [[ ! -e "$SOURCE_DEV" ]]; do
-    echo "Waiting for SD card removal..." | tee -a $LOG_FILE
+    echo "Waiting for SD card removal..."
     sleep 30
   done
 
-  echo "SD card removed, restarting backup script" | tee -a $LOG_FILE
+  echo "SD card removed, restarting backup script"
 fi
